aaf5da4b82c1a15ff2c98a6f3e4e354535ba223b
pam-NUCBA <paminagolthier@gmail.com>
2021-06-28 00:08:03 -0300

    adding user backend

--- server/controllers/usersController.ts
+++ server/controllers/usersController.ts
@@ -7,26 +7,25 @@ import generateToken from 'utils/generateToken.js';
 //api/users/login
 const authUser = expressAsyncHandler(async (req: Request, res: Response) => {
     const {email, password} = req.body
-    //*ahora ts no nos va a dejar hacer esto!
-    res.send({email, password})
+    // res.send({email, password})
 
-    // const user = await User.findOne({email})
+    const user = await User.findOne({email})
 
     // //*ahora para poder usarlo tengo que crear el util de jwt
     // //*este match va a venir del userModel
-    // if (user && (await user.matchPassword(password))) {
-    //     const {_id, name, email, isAdmin} = req.body
-    //     res.json({
-    //       _id,
-    //       name,
-    //       email,
-    //       isAdmin,
-    //       token: generateToken(user._id),
-    //     })
-    //   } else {
-    //     res.status(401)
-    //     throw new Error('Invalid email or password')
-    //   }
+    if (user && (await user.matchPassword(password))) {
+        const {_id, name, email, isAdmin} = req.body
+        res.json({
+          _id,
+          name,
+          email,
+          isAdmin,
+          token: generateToken(user._id),
+        })
+      } else {
+        res.status(401)
+        throw new Error('Invalid email or password')
+      }
 })
 
 
